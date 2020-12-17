97abae176e5015ab681452b91e0a62580734eedd
unknown <sindrema@gmail.com>
2020-12-17 23:31:02 +0100

    started on message handler

--- handlers/groupMakerHandler.js
+++ handlers/groupMakerHandler.js
@@ -9,7 +9,7 @@ const { DMChannel } = require("discord.js");
 module.exports = {
     createNewGroup(user, channel, messageTemplate = 1) {
         return new Promise((resolve, reject) => {
-            this.deleteOldGroupsCreations(user, channel).then(() => {
+            this.deleteMessageSetup(user, channel).then(() => {
                 //old creation have been deleted if it existed
 
                 const payload = {
@@ -19,33 +19,7 @@ module.exports = {
                     template: messageTemplate
                 }
                 sql.insertMessageSetup(payload).then(() => {
-                    console.log("1");
-                    sql.getMessageTemplates().then(templates => {
-                        console.log("templates", templates);
-                        const template = templates.filter(c => c.id === messageTemplate).length !== 0 ? templates.filter(c => c.id === messageTemplate)[0] : null
-                        if (template) {
-                            sql.getMessages(template.id).then(messages => {
-                                console.log("user", user);
-                                user.createDM().then(DMChannel => {
-                                    if (true) {
-                                        var embed = ac.embed(DMChannel, `Starting group creation for: ${template.name}`, ``, null, true);
-                                        DMChannel.send(embed)
-                                    }
-                                    if (true) {
-                                        var embed = ac.embed(DMChannel, messages[0].title, ``, null, true);
-                                        DMChannel.send(embed)
-                                    }
-                                })
-                            }).catch(x => {
-                                reject(x)
-                            })
-                        } else {
-                            reject("The message template you are trying to use does not exist!")
-                        }
-                    }).catch(x => {
-                        logger.error(x)
-                        reject(x)
-                    })
+                    this.sendSetupMessage(user);
                 }).catch(x => {
                     logger.error(x)
                     reject(x)
@@ -57,7 +31,7 @@ module.exports = {
             })
         })
     },
-    deleteOldGroupsCreations(user, channel) {
+    deleteMessageSetup(user, channel) {
         return new Promise((resolve, reject) => {
             sql.getMessageSetup(user.id).then(setups => {
                 console.log("setups", setups);
@@ -74,5 +48,57 @@ module.exports = {
                 }
             }).catch(x => reject(x))
         })
+    },
+    sendSetupMessage(user, channel) {
+        sql.getMessageSetup(user.id).then(setups => {
+            const filteredSetups = setups.filter(x => x.completed && user.id === x.user && channel.id === x.channel)
+            if (filteredSetups.length !== 0) {
+                const setup = filteredSetups[0]
+                return sql.getMessageTemplates().then(templates => {
+                    console.log("templates", templates);
+                    const template = templates.filter(c => c.id === messageTemplate).length !== 0 ? templates.filter(c => c.id === setup.template)[0] : null
+                    if (template) {
+                        sql.getMessages(template.id).then(messages => {
+
+                            sql.getMessageProgress(setup.id).then(progresses => {
+
+                                const filteredProgresses = progresses.filter(x => x.completed).sort((a, b) => a.message_id - b.message_id).sort((a, b) => a.id - b.id)
+                                let messageProgress = 0
+                                if (filteredProgresses.length !== 0) {
+                                    messageProgress = filteredProgresses[filteredProgresses.length - 1]
+                                }
+
+
+
+                                const newMsg = messages[messageProgress]
+
+
+                                console.log("user", user);
+                                user.createDM().then(DMChannel => {
+                                    if (true) {
+                                        var embed = ac.embed(DMChannel, `Starting group creation for: ${template.name}`, ``, null, true);
+                                        DMChannel.send(embed)
+                                    }
+                                    if (true) {
+                                        var embed = ac.embed(DMChannel, messages[0].title, ``, null, true);
+                                        DMChannel.send(embed)
+                                    }
+                                })
+
+                            }).catch(X => logger.error(x))
+
+                        }).catch(x => {
+                            reject(x)
+                        })
+                    } else {
+                        reject("The message template you are trying to use does not exist!")
+                    }
+                }).catch(x => {
+                    logger.error(x)
+                    reject(x)
+                })
+            }
+        }).catch(x => logger.error(x))
     }
+
 }
